f67f649617aefbbfb415c9407b40bb95593b9803
Amber Stumpf <aiyemacbain@gmail.com>
2019-05-21 17:20:15 -0700

    Add onChange to component

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Card, CardTitle, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 class AddRecipe extends Component {
     state = {
         title: '',
         ingredients: '',
-        directions: ''
+        directions: '',
+
     }
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
 
 
     render() {
+        const { title, ingredients, directions } = this.state
         return (
-            <Form className='container'>
-                <h2>Add a Recipe</h2>
-                <FormGroup>
-                    <Label>Title</Label>
-                    <Input placeholder='Enter Recipe Name'></Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label>Ingredients</Label>
-                    <Input placeholder='List ingredients and amounts'></Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label></Label>
-                    <Input></Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label>Directions</Label>
-                    <Input placeholder='Directions and notes'></Input>
-                    <FormText></FormText>
-                </FormGroup>
-            </Form>
+            <div className='container'>
+                <Card className='m-3'>
+                    <CardTitle><h2>Add a Recipe</h2></CardTitle>
+                    <Form className='mx-2'>
+                        <FormGroup>
+                            <Label for='title'>Title</Label>
+                            <Input
+                                value={title}
+                                type='text'
+                                name='title'
+                                placeholder='Enter Recipe Name'
+                                onChange={this.onChange}
+                            ></Input>
+                        </FormGroup>
+                        {/*<FormGroup>
+                            <Label for='image'>Picture?</Label>
+                            <Input
+                                value={image}
+                                type='image'
+                                name=''
+                                placeholder='select image'
+                                onChange={this.onChange}
+                            ></Input>
+                        </FormGroup>*/}
+                        <FormGroup>
+                            <Label for='ingredients'>Ingredients</Label>
+                            <Input
+                                value={ingredients}
+                                type='text'
+                                name='ingredients'
+                                placeholder='List ingredients and amounts'
+                                onChange={this.onChange}
+                            ></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='directions'>Directions</Label>
+                            <Input
+                                value={directions}
+                                type='textarea'
+                                name='directions'
+                                placeholder='Directions and notes'
+                                onChange={this.onChange}
+                            ></Input>
+                        </FormGroup>
+                        <Button className='mb-2 btn-light'>Submit Recipe</Button>
+                    </Form>
+                </Card>
+            </div>
         )
     }
 }
